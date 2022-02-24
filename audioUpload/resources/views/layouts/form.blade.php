7ff8eafaafcb02f467df2d5a1cb2739cdf3ca608
Tounani Abderrahmane <tounaniabderrahmane99@gmail.com>
2022-02-25 00:12:55 +0100

    feat (main) : add migration and model House

--- audioUpload/resources/views/layouts/form.blade.php
+++ audioUpload/resources/views/layouts/form.blade.php
@@ -4,34 +4,35 @@
 
 <div class=" min-h-screen flex items-center">
    <div class="w-full">
-     <h2 class="text-center text-blue-400 font-bold text-2xl uppercase mb-10">add some music</h2>
+     <h2 class="text-center text-blue-400 font-bold text-2xl uppercase mb-10">add House</h2>
      <div class="bg-white p-10 rounded-lg shadow md:w-3/4 mx-auto lg:w-1/2">
-       <form action="">
+       <form action="{{Route('house.store')}}" method="POST" enctype="multipart/form-data">
+         @csrf
          <div class="mb-5">
-           <label for="name" class="block mb-2 font-bold text-gray-600">title</label>
-           <input type="text" id="name" name="name" placeholder="Put in your fullname." class="border border-gray-300 shadow p-3 w-full rounded mb-">
+           <label for="place" class="block mb-2 font-bold text-gray-600">place</label>
+           <input type="text" required id="place" name="place" placeholder="place" class="border border-gray-300 shadow p-3 w-full rounded mb-">
          </div>
 
          <div class="mb-5">
-           <label for="twitter" class="block mb-2 font-bold text-gray-600">signer</label>
-           <input type="text" id="twitter" name="twitter" placeholder="Put in your fullname." class="border border-gray-300 shadow p-3 w-full rounded mb-">
+           <label for="description" class="block mb-2 font-bold text-gray-600">description</label>
+           <input type="text" required id="description" name="description" placeholder="description" class="border border-gray-300 shadow p-3 w-full rounded mb-">
            
          </div>
          <div class="mb-5">
-           <label for="image" class="block mb-2 font-bold text-gray-600">image signer</label>
-           <input type="file" id="image" name="image"  class="border border-gray-300 shadow p-3 w-full rounded mb-">
+           <label for="image" class="block mb-2 font-bold text-gray-600">image of the House</label>
+           <input type="file" required id="image" name="image"  class="border border-gray-300 shadow p-3 w-full rounded mb-">
            
          </div>
          
          <div class="mb-5">
-           <label for="music" class="block mb-2 font-bold text-gray-600">link</label>
-           <input type="file" id="music" name="music"  class="border border-red-300 shadow p-3 w-full rounded mb-">
+           <label for="tage" class="block mb-2 font-bold text-gray-600">nombre des tages</label>
+           <input type="text" required id="tage" name="tage"  class="border border-red-300 shadow p-3 w-full rounded mb-">
           
          </div>
 
          <div class="mb-5">
-           <label for="price" class="block mb-2 font-bold text-gray-600">price</label>
-           <input type="number" id="price" name="price"  class="border border-gray-300 shadow p-3 w-full rounded mb-">
+           <label for="price"  class="block mb-2 font-bold text-gray-600">price</label>
+           <input type="number"  required id="price" name="price"  class="border border-gray-300 shadow p-3 w-full rounded mb-">
            
          </div>
 
